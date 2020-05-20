8d74309d61556a4b8b3c65a9a6821137e166c83b
Quang Dinh Le <lequangdinh@gmail.com>
2020-05-20 10:33:46 +0700

    Update to use MaterialDark

---
+++ configs/blinksh-themes/MaterialDark.js
@@ -0,0 +1,27 @@
+black       = '#252525'; // black (coal)
+red         = '#FF443E'; // red
+green       = '#C3D82C'; // green (turqoise)
+yellow      = '#FFC135'; // yellow (orange)
+blue        = '#42A5F5'; // blue (peter river)
+magenta     = '#D81B60'; // magenta (alizarin)
+cyan        = '#00ACC1'; // cyan
+white       = '#F5F5F5'; // white (clouds)
+lightBlack  = '#5D6F71'; // light black (gravel)
+lightRed    = '#FF443E'; // light red (pumpkin)
+lightGreen  = '#C3D82C'; // light green (emerald)
+lightYellow = '#FFC135'; // light yellow (dirtyblonde)
+lightBlue   = '#42A5F5'; // light blue
+lightMagenta= '#D81B60'; // light magenta (alizarin)
+lightCyan   = '#00ACC1'; // light cyan
+lightWhite  = '#F5F5F5'; // white (snow)
+
+t.prefs_.set('color-palette-overrides', [
+  black,      red,      green,      yellow,      blue,      magenta,      cyan,      white,
+  lightBlack, lightRed, lightGreen, lightYellow, lightBlue, lightMagenta, lightCyan, lightWhite
+]);
+
+t.prefs_.set('cursor-blink', true);
+t.prefs_.set('cursor-color', '#FF443E');
+
+t.prefs_.set('foreground-color', '#A1B0B8');
+t.prefs_.set('background-color', '#151515');
