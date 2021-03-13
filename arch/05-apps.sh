e868a6b482fab0450e8da064c653a977f287d218
Quang Dinh Le <lequangdinh@gmail.com>
2021-03-13 19:42:42 +0700

    Split apps & Golang

--- arch/05-apps.sh
+++ arch/05-apps.sh
@@ -20,28 +20,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
   sh aur.sh onlyoffice-bin
 fi
 
-read -p "Do you want to install Postman? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sh aur.sh postman-bin
-fi
-
-read -p "Do you want to install Visual Studio Code? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sh aur.sh visual-studio-code-bin
-fi
-
 read -p "Do you want to install Extreme Download Manager? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   sh aur.sh xdman
-fi
-
-read -p "Do you want to install Android Studio? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sh aur.sh android-studio
 fi
 
 read -p "Do you want to install 1Password Manager? " -n 1 -r
@@ -49,12 +31,6 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   gpg --keyserver keyserver.ubuntu.com --recv-keys 3FEF9748469ADBE15DA7CA80AC2D62742012EA22
   sh aur.sh 1password
-fi
-
-read -p "Do you want to install Nodejs? " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sudo pacman -S --noconfirm nodejs npm
 fi
 
 read -p "Do you want to install Telegram Desktop? " -n 1 -r
---
+++ arch/06-coding.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+read -p "Do you want to install Postman? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sh aur.sh postman-bin
+fi
+
+read -p "Do you want to install Visual Studio Code? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sh aur.sh visual-studio-code-bin
+fi
+
+read -p "Do you want to install Android Studio? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sh aur.sh android-studio
+fi
+
+read -p "Do you want to install Nodejs? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo pacman -S --noconfirm nodejs npm
+fi
+
+
+read -p "Do you want to install Docker & Docker Compose? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo pacman -S --noconfirm docker docker-compose
+  current_user=$(whoami)
+  sudo usermod -aG docker &current_user
+  sudo systemctl enable docker
+fi
+
+
+read -p "Do you want to install Golang? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  curl -L https://golang.org/dl/go1.16.2.linux-amd64.tar.gz -o go.tar.gz
+  tar xvpf go.tar.gz
+  sudo rm -rf /usr/local/go
+  sudo mv go /usr/local
+  rm -rf go.tar.gz
+fi
