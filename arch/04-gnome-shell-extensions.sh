4fb8066172206352d934a609d86279179442e4bd
Quang Dinh Le <lequangdinh@gmail.com>
2021-03-15 21:56:27 +0700

    Switched to topicons

--- arch/04-gnome-shell-extensions.sh
+++ arch/04-gnome-shell-extensions.sh
@@ -3,8 +3,8 @@
 echo Installing gnome-shell-extension-dash-to-dock
 sh aur.sh gnome-shell-extension-dash-to-dock
 
-echo Installing gnome-shell-extension-tray-icons
-sh aur.sh gnome-shell-extension-tray-icons
+echo Installing gnome-shell-extension-topicons-plus
+sh aur.sh gnome-shell-extension-topicons-plus
 
 echo Installing gnome-shell-extension-desktop-icons-ng
 sh aur.sh gnome-shell-extension-desktop-icons-ng
