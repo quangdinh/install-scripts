acdb2f605e1a8f0a5f55e93ae7cf4575b2221c03
Quang Dinh Le <lequangdinh@gmail.com>
2021-03-15 13:18:53 +0700

    Update ifuse

--- arch/09-apple.sh
+++ arch/09-apple.sh
@@ -5,6 +5,7 @@ file_name="/usr/local/bin/mounti"
 read -p "Do you want to install Apple mount script? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sh aur.sh ifuse
   sudo cp ./automount-iphone.sh $file_name
   sudo chmod +x $file_name
 fi
