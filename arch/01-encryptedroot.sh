a0d1a5f043bb3dddc089000410aa9e34b153fa46
Quang Dinh Le <qdtc@Carbon.local>
2021-04-04 14:58:54 +0700

    Fixed crypt

--- arch/01-encryptedroot.sh
+++ arch/01-encryptedroot.sh
@@ -9,10 +9,12 @@ if [ -z "$uuid" ]; then
   echo $encrypted_var is not an encrypted devices
 else
   root_partition=$(findmnt / -o uuid | tail -n 1)
-  echo $root_partition
-  sed -i -e "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet cryptdevice=UUID=$uuid:cryptlvm root=UUID=$root_partition\"/g" /etc/default/grub
-  sed -i -e 's/HOOKS=(.*)/HOOKS=(base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck)/g' /etc/mkinitcpio.conf
-  sleep 1
+  sed -e "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet cryptdevice=UUID=$uuid:cryptlvm root=UUID=$root_partition\"/g" /etc/default/grub > grub #/etc/default/grub
+  sed -e 's/HOOKS=(.*)/HOOKS=(base udev autodetect keyboard modconf block encrypt lvm2 filesystems fsck)/g' /etc/mkinitcpio.conf > mkinitcpio.conf
+  cp grub /etc/default/grub
+  rm -rf grub
+  cp mkinitcpio.conf /etc/mkinitcpio.conf
+  rm -rf mkinitcpio.conf
   mkinitcpio -P
   grub-mkconfig -o /boot/grub/grub.conf
 fi
--- arch/04-gnome-shell-extensions.sh
+++ arch/04-gnome-shell-extensions.sh
@@ -5,8 +5,6 @@ sh aur.sh install -y gnome-shell-extension-dash-to-dock
 
 sh aur.sh install -y gnome-shell-extension-topicons-plus
 
-sh aur.sh install -y gnome-shell-extension-desktop-icons-ng
-
 sh aur.sh install -y gnome-shell-extension-sound-output-device-chooser
 
 sh aur.sh install -y gnome-shell-extension-clipboard-indicator
