da988721d9d375ecca06fb9949628b5c7567f701
Quang Dinh Le <lequangdinh@gmail.com>
2021-09-23 11:22:14 +0700

    Greetd

---
+++ arch-install-sway/after_install/greetd.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -e
+
+read -p "Do you want to setup greetd with tuigreet " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  yay -S --noconfirm greetd greetd-tuigreet
+  cat <<EOF | sudo tee /etc/greetd/config.toml
+[terminal]
+vt = 1
+
+[default_session]
+command = "tuigreet -t --cmd /usr/bin/startsway"
+user = "greeter"
+EOF
+
+  cat <<EOF | sudo tee /usr/bin/startsway
+#!/bin/sh
+
+export XDG_SESSION_TYPE=wayland
+export DESKTOP_SESSION=sway
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt5ct
+export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+if [[ -f \$HOME/.xprofile ]]; then
+	source \$HOME/.xprofile
+fi
+
+exec sway
+EOF
+  sudo chmod +x /usr/bin/startsway
+
+  sudo systemctl enable greetd
+fi
---
+++ arch-install-sway/after_install/misc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xfconf-query --channel thunar --property /misc-full-path-in-title --create --type bool --set true
