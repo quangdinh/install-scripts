4533e79f75b7b0ca5b4c8a78fc88a38452fbb370
Quang Dinh Le <lequangdinh@gmail.com>
2021-09-25 10:46:20 +0700

    Fixed typo

--- arch-install-sway/after_install/neomutt.sh
+++ arch-install-sway/after_install/neomutt.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-read -p "Do you want to install Ymuse? " -n 1 -r
+read -p "Do you want to install Neomutt & Lynx? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   sudo pacman -S neomutt lynx
