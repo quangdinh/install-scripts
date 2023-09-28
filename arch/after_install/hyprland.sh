661970256a9364302681b3002f9db37d991763cb
Quang Dinh Le <lequangdinh@gmail.com>
2023-09-28 12:25:57 +0200

    Add ly to arch

--- arch/after_install/hyprland.sh
+++ arch/after_install/hyprland.sh
@@ -4,5 +4,6 @@ set -e
 
 
 sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi alacritty \
-  imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-hyprland mako 
+  imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-hyprland mako ly
 sudo ./gnome_keyring.py
+sudo systemctl enable ly.service
