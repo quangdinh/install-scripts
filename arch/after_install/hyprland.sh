036c414e39e2359cb9bbf67196d8e43b5bc4fbaf
Quang Dinh Le <lequangdinh@gmail.com>
2023-09-28 23:07:39 +0200

    Switch alacritty for kitty

--- arch/after_install/hyprland.sh
+++ arch/after_install/hyprland.sh
@@ -3,7 +3,7 @@
 set -e
 
 
-sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi alacritty \
+sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi kitty \
   xdg-user-dirs-gtk imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-gtk \
   xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify
 sudo ./gnome_keyring.py
