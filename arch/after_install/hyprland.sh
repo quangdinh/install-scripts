c44c4259dbb90beb07c29898867f418083ecc105
Quang Dinh Le <lequangdinh@gmail.com>
2023-09-29 10:51:10 +0200

    Add hyprshot to hyprland

--- arch/after_install/hyprland.sh
+++ arch/after_install/hyprland.sh
@@ -5,7 +5,8 @@ set -e
 
 sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi kitty \
   xdg-user-dirs-gtk imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-gtk \
-  xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify
+  xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify jq python-pillow
+yay -S --noconfirm hyprshot
 sudo ./gnome_keyring.py
 sudo systemctl enable ly.service
 sudo ./hide_system_apps.sh
