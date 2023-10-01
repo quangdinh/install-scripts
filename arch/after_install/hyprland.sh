1394509f3d3d5471996d128b8ea95d35e2031595
Quang Dinh Le <lequangdinh@gmail.com>
2023-10-01 21:56:50 +0200

    Update screenshot tool

--- arch/after_install/hyprland.sh
+++ arch/after_install/hyprland.sh
@@ -5,9 +5,11 @@ set -e
 
 sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi kitty \
   xdg-user-dirs-gtk imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-gtk \
-  xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify jq python-pillow
-yay -S --noconfirm hyprshot
+  xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify jq \
+  python-pillow swappy
 sudo ./gnome_keyring.py
 sudo systemctl enable ly.service
 sudo ./hide_system_apps.sh
 ./ranger.sh
+sudo mkir -p /usr/local/bin
+sudo cp ./hyprland/* /usr/local/bin/
