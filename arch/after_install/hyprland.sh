dc541637f3475fa2e3e4982e7dfdb17ebe36474e
Quang Dinh Le <lequangdinh@gmail.com>
2023-10-02 22:55:11 +0200

    Remove pillow

--- arch/after_install/hyprland.sh
+++ arch/after_install/hyprland.sh
@@ -6,7 +6,7 @@ set -e
 sudo pacman -S --noconfirm hyprland hyprpaper swaylock swayidle waybar wofi kitty \
   xdg-user-dirs-gtk imv zathura zathura-pdf-poppler mpv xdg-desktop-portal-gtk \
   xdg-desktop-portal-hyprland mako ly slurp grim wl-clipboard libnotify jq \
-  python-pillow swappy polkit-gnome
+  swappy polkit-gnome
 sudo ./gnome_keyring.py
 sudo systemctl enable ly.service
 sudo ./hide_system_apps.sh
