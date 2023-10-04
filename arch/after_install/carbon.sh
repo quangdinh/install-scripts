41a90f06151a32c0e01cf75bbb2b5cec08dc1fd8
Quang Dinh Le <lequangdinh@gmail.com>
2023-10-04 14:21:13 +0200

    Update

---
+++ arch/after_install/carbon.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+./reflector.sh
+./thinkpad.sh
+./firewall.sh
+./hyprland.sh
+yay -Syu --noconfirm rbenv ruby-build nvm 1password mongodb-compass \
+  libfreoffice-fresh postman-bin slack-desktop-wayland google-chrome
